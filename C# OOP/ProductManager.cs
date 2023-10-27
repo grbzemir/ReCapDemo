a628d42bec5295c97ee11db15ff59fc9dbf391c2
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2023-10-28 00:30:58 +0300

    Proje dosyası ekle.

---
+++ C# OOP/ProductManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__OOP
+{
+    class ProductManager
+
+    {
+
+        public void Add()
+
+        {
+
+            Console.WriteLine("Product Added!");
+
+        }
+
+        public void Update()
+
+        {
+
+            Console.WriteLine("Product Updated!");
+
+        }
+    }
+}
+
+
---
+++ ReCapDemo/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReCapDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Button[,] buttons= new Button[8,8];
+            
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = i * 50;
+                    buttons[i, j].Top = j * 50;
+                    this.Controls.Add(buttons[i, j]);
+                }
+            }   
+            this.Controls.Add(buttons);
+            
+        }
+    }
+}
