804cbea4c6fceec4b34c60f7c25db585f81dd311
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2023-10-28 00:50:11 +0300

    Buttons Set

--- ReCapDemo/Form1.cs
+++ ReCapDemo/Form1.cs
@@ -20,22 +20,76 @@ namespace ReCapDemo
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            Button[,] buttons= new Button[8,8];
-            
-            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+
+            Button[,] buttons = new Button[8, 8];
+
+            int top;
+            int left;
+
+            left = 0;
+            top = 0;
+
+            int i;
+
+            for (i = 0; i < buttons.GetUpperBound(0); i++) // 0. Boyutun Son Elemanı
+
             {
-                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+
+                int j;
+
+                for (j = 0; j <= buttons.GetUpperBound(1); j++)  // 1.Boyutun Son Elemanı
+
+
+
                 {
+
                     buttons[i, j] = new Button();
                     buttons[i, j].Width = 50;
                     buttons[i, j].Height = 50;
-                    buttons[i, j].Left = i * 50;
-                    buttons[i, j].Top = j * 50;
+
+                    buttons[i, j].Top = top;
+                    buttons[i, j].Left = left;
+
+                    left = left + 50;
+
+                    if ((i + j) % 2 == 0)
+
+                    {
+
+
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+
+
+                    else
+
+                    {
+
+
+                        buttons[i, j].BackColor = Color.White;
+                    }
+
                     this.Controls.Add(buttons[i, j]);
+
+
                 }
-            }   
-            this.Controls.Add(buttons);
-            
+
+                top = top + 50;
+                left = 0;
+
+
+            }
+
+
         }
+
+
     }
+
+
+
 }
